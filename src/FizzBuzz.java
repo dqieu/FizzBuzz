77a51cd7667638960bd49068b2ce013532a08962
dqieu <tiendat691@gmail.com>
2023-09-11 10:34:21 -0400

    task 3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,7 +4,12 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
-        int i = 0;
+        doFizzBuzz(5);
+
+    }
+
+    public static void doFizzBuzz(int i) {
+
         while (i < 100) {
 
             // Find out which numbers divide i.
